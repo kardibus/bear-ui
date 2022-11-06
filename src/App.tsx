ba08bb0561ef9f2deb87e6b2bbc639d141ee3649
kardibus <kardibus@gmail.com>
2022-11-06 18:58:27 +0500

    замена хоста

--- src/App.tsx
+++ src/App.tsx
@@ -47,8 +47,17 @@ function App() {
                     Accept: 'application/json',
                 },
             });
-        } catch (e) {
-
+            if (!response.ok) {
+                throw new Error(`Error! status: ${response.status}`);
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                console.log('error message: ', error.message);
+                return error.message;
+            } else {
+                console.log('unexpected error: ', error);
+                return 'An unexpected error occurred';
+            }
         }
     }
 }
